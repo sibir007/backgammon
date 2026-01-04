e297988cce1c8ee5eaa9f2434c9fe77bf4d88de6
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2026-01-04 16:57:46 +0300

    add axios

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,16 +1,15 @@
 import { Box, Flex, Heading, Spacer, VStack, Text, HStack } from '@chakra-ui/react';
 import { useWallet } from "@solana/wallet-adapter-react"
-import WalletMultiButton from "@/components/old-components/WalletMultiButton"
-import DisplayGameState from "@/components/old-components/DisplayGameState"
-import InitPlayerButton from "@/components/old-components/InitPlayerButton"
-import SessionKeyButton from "@/components/old-components/SessionKeyButton"
-import ChopTreeButton from "@/components/old-components/ChopTreeButton"
-import RequestAirdrop from "@/components/old-components/RequestAirdrop"
-import DisplayNfts from "@/components/old-components/DisplayNfts"
-import { useState } from "react"
-import { initialGameState } from "../test-data/game-data"
+// import WalletMultiButton from "@/components/old-components/WalletMultiButton"
+// import DisplayGameState from "@/components/old-components/DisplayGameState"
+// import InitPlayerButton from "@/components/old-components/InitPlayerButton"
+// import SessionKeyButton from "@/components/old-components/SessionKeyButton"
+// import ChopTreeButton from "@/components/old-components/ChopTreeButton"
+// import RequestAirdrop from "@/components/old-components/RequestAirdrop"
+// import DisplayNfts from "@/components/old-components/DisplayNfts"
+// import { useState } from "react"
 import { GameComponent } from '@/components/my-components/game_two/Game';
-import { BGameStateProvider as TTTGameStateProvider_2 } from '@/contexts/BGameStateProvider_2'
+// import { BGameStateProvider as TTTGameStateProvider_2 } from '@/contexts/BGameStateProvider_2'
 // import { Game_2 } from '@/components/my-components/game_two/Game_2';
 
 export default function Home() {
